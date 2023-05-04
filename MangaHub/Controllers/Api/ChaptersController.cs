672a3a49abab1d6d19936312ff147b5767e4952b
farouk subair <farouk_subair@yahoo.com>
2023-05-04 16:58:15 +0100

    adjust Manga Index View

--- MangaHub/Controllers/Api/ChaptersController.cs
+++ MangaHub/Controllers/Api/ChaptersController.cs
@@ -14,7 +14,7 @@ namespace MangaHub.Controllers.Api
         public ChaptersController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-        }
+         }
 
         [HttpPost]
         public IHttpActionResult Add(ChapterDto dto)
